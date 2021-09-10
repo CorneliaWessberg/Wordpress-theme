4cdb6a71e48686eba5ef5eb215347c6289d21122
[CorneliaWessberg] <cornelia.wessnass@gmail.com>
2021-09-10 15:21:53 +0200

    first commit

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php 
+get_header();
+?>
+
+<head>
+<?php wp_head();?>
+</head>
+
+<main>
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="hero">
+                            <img src="<?php bloginfo('template_directory');?>/img/img/city.jpg" alt="city"/>
+								<div class="text">
+								<h1><?php the_title(); ?></h1>
+                                <p><?php the_content(); ?></p>
+                                
+								
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+
+
+<?php
+get_footer();
+?>
